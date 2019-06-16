3e307b0cc78f8fe28dc92e02b8b0369df638cb49
Nellie <elenagithub@gmail.com>
2019-06-16 10:13:13 -0400

    final version

--- mod_files.py
+++ mod_files.py
@@ -1,6 +1,3 @@
-import pandas as pd
-
-
 def clear_csv_file(filename):
     f = open(filename, "w")
     f.close()
@@ -10,8 +7,4 @@ def write_column_titles(filename):
     with open(filename, "w") as f:
         f.write("{}".format("IP_address,Port,Country_City,Speed,Type,Anonymity,Last_Check"))
 
-
-def remove_empty_lines(input_csv_file, output_csv_file):
-    df = pd.read_csv(input_csv_file)
-    df.to_csv(output_csv_file, index=False)
 
--- mod_main.py
+++ mod_main.py
@@ -5,11 +5,12 @@ from mod_files import *
 def main():
     file = './proxy.csv'
     url = 'https://hidemyna.me/en/proxy-list/'
-    num_pages_to_search = 2
+    num_pages_to_search = 3
 
     clear_csv_file(file)
     navigate_to_page(url)
     do_scraping(num_pages_to_search, file)
+
 
 if __name__ == '__main__':
     main()
